2c7a2793b630a61de10373190ef2114cb78a9993
StagoAce <99208572+StagoAce@users.noreply.github.com>
2024-05-06 11:00:02 -0500

    version 0.2

---
+++ demo/src/main/java/com/example/demo/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("nombre", "Juan Perez"); // Ejemplo de dato
+        return "index"; // Nombre de la plantilla en templates
+    }
+}
