0dc17c79e2fd95aaeda77c45532506ab38ea1e19
StagoAce <99208572+StagoAce@users.noreply.github.com>
2024-05-08 17:05:43 -0500

    version 0.4

--- demo/src/main/java/com/example/demo/controllers/HomeController.java
+++ demo/src/main/java/com/example/demo/controllers/HomeController.java
@@ -4,13 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
 
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("nombre", "Juan Perez"); // Ejemplo de dato
-        return "index"; // Nombre de la plantilla en templates
+        return "Hello World"; // Nombre de la plantilla en templates
     }
 }
